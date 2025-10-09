370d35dde61a8ecf0963c3d2194f6701adefa403
HuneyDev <alexlp4pi@gmail.com>
2025-10-08 19:44:15 -0700

    Add grid-based map and player movement system
    Introduces a 3x3 grid map with randomly generated rooms, and implements player movement within the map using directional commands. Refactors room entry and search logic to work with the new map structure, and updates room classes to support the new system.

--- GameManager.cs
+++ GameManager.cs
@@ -17,6 +17,14 @@
             }
         }
 
+        Room[,] house = new Room[3, 3];
+
+        private Room[,] map;
+        private int rows = 3;
+        private int cols = 3;
+        private (int row, int col) playerPos;
+        private Random rng = new Random();
+
         static void Main(string[] args)
         {
             GameManager gameManager = new GameManager();
@@ -117,6 +125,9 @@
 
         public void ExploreRooms()
         {
+            GenerateMap();
+
+
             //begginer dice
             Inventory.Add(20);
             Inventory.Add(16);
@@ -140,17 +151,25 @@
             bool exploring = true;
             while (exploring)
             {
+                Room selectedRoom = map[playerPos.row, playerPos.col];
+                currentRoom.EnterRoom();
+
                 Console.WriteLine("What next?");
                 string cmd = Console.ReadLine().ToLower();
 
                 switch (cmd)
                 {
                     case "n":
+                        MovePlayer(-1, 0);
+                        break;
                     case "e":
+                        MovePlayer(0, 1);
+                        break;
                     case "s":
+                        MovePlayer(1, 0);
+                        break;
                     case "w":
-                        currentRoom = currentRoom.Move(cmd);
-                        currentRoom.EnterRoom();
+                        MovePlayer(0, -1);
                         break;
                     case "search":
                         Console.WriteLine(currentRoom.RoomSearch(this));
@@ -167,6 +186,67 @@
                         break;
                 }
             }
+        }
+
+        private void GenerateMap()
+        {
+            map = new Room[rows, cols];
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    int roll = rng.Next(1, 3);
+
+                    Room room;
+                    if (roll == 1)
+                    {
+                        room = new TreasureRoom();
+                    }
+                    else if (roll == 2)
+                    {
+                        room = new EncounterRoom();
+                    }
+                    else
+                    {
+                        room = new Center();
+
+                        
+                    }
+                        map[r, c] = room;
+                }
+            }
+
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    Room room = map[r, c];
+                    room.North = (r > 0) ? map[r - 1, c] : null;
+                    room.South = (r < rows - 1) ? map[r + 1, c] : null;
+                    room.West = (c > 0) ? map[r, c - 1] : null;
+                    room.East = (c < cols - 1) ? map[r, c + 1] : null;
+                }
+            }
+
+            playerPos = (rng.Next(rows), rng.Next(cols));
+        }
+
+        private void MovePlayer(int dRow, int dCol)
+        {
+            int newRow = playerPos.row + dRow;
+            int newCol = playerPos.col + dCol;
+
+            if (newRow < 0 || newRow >= rows || newCol < 0 || newCol >= cols)
+            {
+                Console.WriteLine("You would go that way normally, but someone put a wall in the way.");
+                return;
+            }
+
+            Room currentRoom = map[playerPos.row, playerPos.col];
+            currentRoom.RoomExit();
+
+            playerPos = (newRow, newCol);
         }
 
         //checks who has the least amount of turns and declares winner
@@ -200,6 +280,7 @@
 
         public abstract class Room
         {
+            public Room North, South, East, West;
             //each room connects to other rooms
             public Dictionary<String, Room> Exits { get; set; } = new();
 
@@ -277,13 +358,14 @@
             }
         }
 
-       public class TreasureRoom : Room
+        public class TreasureRoom : Room
         {
             private bool treasureTaken = false;
             public TreasureRoom() { nameof = "Treasure Room"; }
             public override string RoomDescription() => "You are in the treasure room of the house.";
-            public override void EnterRoom()
+            public override string RoomEntered(GameManager game)
             {
+                Console.WriteLine(RoomDescription());
                 if (!beenHere)
                 {
                     Console.WriteLine($"You enter the {nameof}, for the first time. Why do you have this in your house?");
@@ -293,41 +375,55 @@
                 {
                     Console.WriteLine($"You return to the {nameof}.");
                 }
+                return $"You {(beenHere ? "return to" : "enter")} the {nameof}.";
             }
             public override string RoomSearch(GameManager game)
             {
                 if (!treasureTaken)
                 {
+                    int[] treasure = { 4, 6, 8, 20 };
+                    Random rng = new Random();
+                    int newDie = treasure[rng.Next(treasure.Length)];
+                    Console.WriteLine($"You look around and find a d{newDie}, you take it.");
+                    game.Inventory.Add(newDie);
                     treasureTaken = true;
-                    Console.WriteLine("You found a d8! added to inventory.");
-                    game.Inventory.Add(8);
+                    return $"You found a d{newDie} and added it to your inventory.";
                 }
                 else
                 {
                     Console.WriteLine("You already took the treasure.");
+                    return "You already took the treasure.";
                 }
             }
             public override string RoomExit() => "you leave the treasure room.";
-
-
         }
 
         public class EncounterRoom : Room
         {
             public EncounterRoom() { nameof = "Encounter Room"; }
             public override string RoomDescription() => "You are in the encounter room of the house, wait what?:";
-            public override void EnterRoom(GameManager game)
+            public override string RoomEntered(GameManager game)
             {
-                Console.WriteLine("An appears!");
+                Console.WriteLine(RoomDescription());
+                if (!beenHere)
+                {
+                    Console.WriteLine("You enter the encounter room, for the first time.");
+                    beenHere = true;
+                }
+                else
+                {
+                    Console.WriteLine("You return to the encounter room.");
+                }
+                Console.WriteLine("An enemy appears!");
                 game.PlayGame();
                 beenHere = true;
+                return $"You {(beenHere ? "return to" : "enter")} the {nameof}.";
             }
             public override string RoomSearch(GameManager game)
             {
                 return "You look around and see the body of a dead dice man.";
             }
             public override string RoomExit() => "You leave the encounter room, why do you have a room like this?";
-
         }
     }
 }
